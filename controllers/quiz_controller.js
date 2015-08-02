c97d11c83f2bda4c624186a3eff27c3447a37f47
Jordi Miserachs <jordi.miserachs@gmail.com>
2015-08-03 00:00:13 +0200

    creacion y validacion preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,6 +16,24 @@ exports.load = function(req, res, next, quizId){
 };
 
 
+// GET /quizes
+exports.index = function(req, res){
+	console.log("LLISTAT PREGUNTES");
+
+	var strBusqueda = (req.query.search || null);
+	console.log("strBusqueda: " + strBusqueda);
+	if (strBusqueda === null){
+		strBusqueda = "%";
+	}else{
+		strBusqueda ="%" + strBusqueda.replace(/ /g, '%') + "%";
+	}
+	console.log("strBusqueda: " + strBusqueda);
+	
+	models.Quiz.findAll({where: ["pregunta like ?", strBusqueda]}).then(function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+	}
+	).catch( function(error){ next(error); });
+};
 
 
 
@@ -23,7 +41,7 @@ exports.load = function(req, res, next, quizId){
 // GET /quizes/:id
 exports.show = function(req, res){
 	console.log("ENUNCIAT PREGUNTA");
-	res.render("quizes/show", {quiz: req.quiz})
+	res.render("quizes/show", {quiz: req.quiz, errors: []})
 
 	//models.Quiz.find(req.params.quizId).then(function(quiz){
 	//	res.render('quizes/show', {quiz: quiz});
@@ -39,7 +57,13 @@ exports.answer = function (req, res){
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = "Correcto";
 	}
-	res.render("quizes/answer", {quiz: req.quiz, respuesta: resultado});
+	res.render(
+		"quizes/answer", 
+		{
+			quiz: req.quiz, 
+			respuesta: resultado,
+			errors: []
+		});
 
 	/*
 	models.Quiz.find(req.params.quizId).then(function(quiz){
@@ -54,25 +78,65 @@ exports.answer = function (req, res){
 		}
 	});
 	*/
-
 };
 
 
-// GET /quizes
-exports.index = function(req, res){
-	console.log("LLISTAT PREGUNTES");
+// GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build(		// crea objeto quiz
+			{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
 
-	var strBusqueda = (req.query.search || null);
-	console.log("strBusqueda: " + strBusqueda);
-	if (strBusqueda === null){
-		strBusqueda = "%";
-	}else{
-		strBusqueda ="%" + strBusqueda.replace(/ /g, '%') + "%";
-	}
-	console.log("strBusqueda: " + strBusqueda);
+	res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+
+
+
+// POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build( req.body.quiz );
+	console.log("quiz: " + JSON.stringify(quiz));
+
+	// guarda en DB los campos pregunta y resuesta de quiz
 	
-	models.Quiz.findAll({where: ["pregunta like ?", strBusqueda]}).then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
+/*	 El següent codi es el que proposa el curs, pero no funciona. 
+*	 El programa s'atura a la linea .then (no existeix el metode)
+*	
+*	quiz
+*	.validate()
+*	.then(		//TypeError: Object #<Object> has no method 'then'
+*		function(err){
+*			if (err) {
+*				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+*			} else {
+*				
+*				quiz
+*				.save({fields: ["pregunta", "respuesta"]})	// guarda en DB pregunta y respuesta del quiz
+*				.then(function(){
+*					res.redirect('/quizes');	//res.redirect: redireccion HTTP a la lista de preguntas
+*				})
+*			
+*			}
+*		}
+*	);
+*/
+
+	// Solucio al problema anterior torbada al foro de curs
+
+	var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+	if (errors)
+	{
+		var i=0; 
+		var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+		for (var prop in errors) errores[i++]={message: errors[prop]};
+		res.render('quizes/new', {quiz: quiz, errors: errores});
+	} else {
+		quiz // save: guarda en DB campos pregunta y respuesta de quiz
+		.save({fields: ["pregunta", "respuesta"]})
+		.then( function(){ res.redirect('/quizes')}) ;
 	}
-	).catch( function(error){ next(error); });
+
+
+	
 };
--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,22 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 /* GET creditos page. */
 router.get('/author', function(req, res) {
-  res.render('author');
+  res.render('author', {errors: []});
 });
 
 // Autoload de comandos con :quizId
 router.param("quizId", quizController.load);	// autoload :quizId
 
 //Definicion de rutas de quizes
-router.get('/quizes/'						, quizController.index);
-router.get('/quizes/:quizId(\\d+)'			, quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer'	, quizController.answer);
+router.get('/quizes/'						, quizController.index);	// llistat de preguntes
+router.get('/quizes/:quizId(\\d+)'			, quizController.show);		// formulari pregunta
+router.get('/quizes/:quizId(\\d+)/answer'	, quizController.answer);	// resposta de la pregunta
+router.get('/quizes/new'					, quizController.new);		// formulari nova pregunta
+router.post('/quizes/create'				, quizController.create);	// creacio pregunta
 
 module.exports = router;
