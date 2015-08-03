e0882e9f8eba98ef963d9b33269fc0733fce37bf
Jordi Miserachs <jordi.miserachs@gmail.com>
2015-08-04 00:51:56 +0200

    autenticacion y session

---
+++ controllers/user_controller.js
@@ -0,0 +1,18 @@
+var users = {
+				admin: {username: "admin", password: "1234"},
+				pepe: {username: "pepe", password: "5678"}
+			};
+
+// Comprueba si el usiario esta registrado en users
+// Si autenticacion falla o hay errores se ejecuta callback(error).
+exports.autenticar = function(login, password, callback) {
+	if (users[login]){
+		if (password === users[login].password){
+			callback(null, users[login]);
+		} else {
+			callback(new Error("Password erroneo"));
+		}
+	} else {
+		callback(new Error("No existe el usuario"));
+	}
+};
